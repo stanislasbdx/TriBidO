dbecccdfeec43a6d0f984fecc1c507e245ae53c0
Stanislas Castaybert <perso@stan1712.com>
2021-11-26 13:49:30 +0100

    ✨ feature・Ajout d'une navigation

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,23 @@
 <template>
 	<v-app>
-		<v-app-bar app color="primary" dark>
-			<div class="d-flex align-center">
-				<v-img alt="Vuetify Logo" class="shrink mr-2" contain src="https://infographart.com/ressources/logos/IFGA/LogoIFGA_1_2x_W.png" transition="scale-transition" width="55" />
-				
-				<h1 class="font-weight-thin">{{ $appConfig.name }}</h1>
-			</div>
-		</v-app-bar>
-
 		<v-main>
+			<v-navigation-drawer permanent width="200px" overlay-color="primary">
+				<v-img src="tbo-all.png"></v-img>
+
+				<v-row class="mt-5">
+					<v-col v-for="item in items" :key="item.name" cols="12" offset="4" class="my-10">
+						<v-icon v-if="String($route.path) == item.url" color="primary" large style="cursor: pointer;">
+							{{ item.icon }}
+						</v-icon>
+						<v-icon v-else color="grey" large style="cursor: pointer;" @click="$router.push(item.url)">
+							{{ item.icon }}
+						</v-icon>
+
+						<h5>{{ item.name }}</h5>
+					</v-col>
+				</v-row>
+			</v-navigation-drawer>
+
 			<HelloWorld />
 		</v-main>
 	</v-app>
@@ -25,7 +34,11 @@
 		},
 
 		data: () => ({
-			//
+			items: [
+				{ name: 'Accueil', icon: 'fa fa-list-alt', url: '/' },
+				{ name: 'Favoris', icon: 'far fa-heart', url: '/favorites' },
+				{ name: 'Carte', icon: 'far fa-map', url: '/map' },
+			],
 		}),
 	};
 
