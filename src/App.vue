64bda84d31e70514f0aae07a3f9e58c71a9bdaa6
Stanislas Castaybert <perso@stan1712.com>
2021-12-06 01:09:40 +0100

    ⏪ rollback・🌈 style・Modification de la puissance du glassmorph sur les cards et suppression du border 1px

--- src/App.vue
+++ src/App.vue
@@ -151,11 +151,10 @@
 
 	/* Glassmorphism card effect */
 	.glasscard {
-		background: rgba( 255, 255, 255, 0.45 )!important;
+		background: rgba( 255, 255, 255, 0.65 )!important;
 		box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.37 )!important;
 		backdrop-filter: blur( 4px )!important;
 		-webkit-backdrop-filter: blur( 4px )!important;
-		border: 1px solid rgba( 255, 255, 255, 0.10 )!important;
 	}
 	.glass {
 		background: rgba( 255, 255, 255, 0.10 )!important;
