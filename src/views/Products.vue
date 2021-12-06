118042c671e179a00547a8f8af970c8df3906511
Stanislas Castaybert <perso@stan1712.com>
2021-12-06 01:06:22 +0100

    🖥️ wip・Remplissage de la page produits (parties affichage produits)

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -6,46 +6,129 @@
 			<v-col cols="12" md="9">
 				<v-row>
 					<v-col cols="12">
-						<v-card class="glasscard" outlined>
+						<v-card>
 							<v-card-text>
-								<p>Coucou à tous, y'a 30 produtis</p>
+								<h1 class="font-weight-regular black--text">Enchères en cours</h1>
 							</v-card-text>
 						</v-card>
 					</v-col>
 
-					<v-col v-for="index in 12" :key="index">
-						<v-card class="glasscard" outlined>
-							<v-list-item three-line>
-								<v-list-item-content>
-									<div class="text-overline mb-4">
-										OVERLINE
-									</div>
-									<v-list-item-title class="text-h5 mb-1">
-										Headline 5
-									</v-list-item-title>
-									<v-list-item-subtitle>Greyhound divisely hello coldly fonwderfully</v-list-item-subtitle>
-								</v-list-item-content>
+					<v-col v-for="index in 12" :key="index" cols="12" sm="6" md="4" xl="3">
+						<v-card class="mx-auto glasscard white--text">
+							<v-img height="20vh" src="http://s1.picswalls.com/wallpapers/2014/07/25/basketball-desktop-wallpaper_041014235_92.jpg"></v-img>
 
-								<v-list-item-avatar tile size="80" color="grey"></v-list-item-avatar>
-							</v-list-item>
+							<v-card-title class="pb-0">Ballon de basket</v-card-title>
+
+							<v-card-text>
+								<div class="text-subtitle-1">
+									{{ Math.round(Math.random() * 500).toLocaleString(undefined, {minimumFractionDigits: 2}) }}€ • 32 enchères
+								</div>
+
+								<div>C'est un super ballon de basket basique, mais il est super !</div>
+							</v-card-text>
+
+							<v-divider class="mx-4"></v-divider>
+
+							<v-card-title>Dernières offres</v-card-title>
+
+							<v-card-text>
+								<v-row>
+									<v-col v-for="index in 5" :key="index" class="pb-1 pt-0">
+										<v-chip class="glass">
+											Sarahlpb {{ Math.round(Math.random() * 500).toLocaleString(undefined, {minimumFractionDigits: 2}) }}€
+										</v-chip>
+									</v-col>
+								</v-row>
+							</v-card-text>
 
 							<v-card-actions>
-								<v-btn outlined rounded text>
-									Button
+								<v-btn text @click="placeBid = !placeBid" color="primary">
+									Poser une enchère
 								</v-btn>
+
+								<v-tooltip bottom color="glass">
+									<template v-slot:activator="{ on, attrs }">
+										<v-btn text icon color="accent" v-bind="attrs" v-on="on">
+											<v-icon small>far fa-bell</v-icon>
+										</v-btn>
+									</template>
+									<span>Créer un suivi</span>
+								</v-tooltip>
+								<v-tooltip bottom color="glass">
+									<template v-slot:activator="{ on, attrs }">
+										<v-btn text icon color="pink" v-bind="attrs" v-on="on">
+											<v-icon small>far fa-heart</v-icon>
+										</v-btn>
+									</template>
+									<span>Ajouter aux favoris</span>
+								</v-tooltip>
 							</v-card-actions>
 						</v-card>
 					</v-col>
 				</v-row>
 			</v-col>
 			<v-col cols="12" md="3" order="first" order-md="last">
-				<v-card class="glasscard">
-					<v-card-text>
-						<h1 class="font-weight-regular">Filtres</h1>
-					</v-card-text>
-				</v-card>
+				<v-row>
+					<v-col cols="12">
+						<v-card>
+							<v-card-text>
+								<h1 class="font-weight-regular black--text">Filtres</h1>
+							</v-card-text>
+						</v-card>
+					</v-col>
+
+					<v-col cols="12">
+						<v-card class="glasscard">
+							<v-card-text>
+							</v-card-text>
+						</v-card>
+					</v-col>
+				</v-row>
 			</v-col>
 		</v-row>
+
+		<v-bottom-sheet v-model="placeBid">
+			<v-sheet class="glasscard pa-5" style="padding-left: 15.5vw!important;">
+				<v-row>
+					<v-col cols="12" md="4">
+						<v-card class="mx-auto">
+							<v-img class="white--text align-end" height="200px" src="http://s1.picswalls.com/wallpapers/2014/07/25/basketball-desktop-wallpaper_041014235_92.jpg">
+								<v-card-title class="pb-0">Ballon de basket</v-card-title>
+
+								<v-card-text align="left">
+									<div class="text-subtitle-1">
+										{{ Math.round(price).toLocaleString(undefined, {minimumFractionDigits: 2}) }}€ • 32 enchères
+									</div>
+
+									<div>C'est un super ballon de basket basique, mais il est super !</div>
+								</v-card-text>
+							</v-img>
+						</v-card>
+					</v-col>
+
+					<v-divider vertical class="my-4" v-if="$vuetify.breakpoint.mdAndUp"></v-divider>
+
+					<v-col cols="12" md="8">
+						<template>
+							<h2>Poser une enchère</h2>
+							<p>Souhaitez vous poser une enchère sur ce produit ?</p>
+
+							<v-row class="col-12 pb-3">
+								<v-btn class="ma-3 mt-0" color="secondary" v-for="index in 5" :key="index">{{ Math.round(price + index * 10).toLocaleString(undefined, {minimumFractionDigits: 2}) }}€ (+{{index}}0€)</v-btn>
+							</v-row>
+
+							<span class="font-italic">Cliquer sur le bouton vous enverra sur la page de paiement.</span>
+						</template>
+
+						<v-row style="position: absolute; bottom: 20px; right: 20px;">
+							<v-btn class="mt-6" text color="red" @click="placeBid = !placeBid">
+								Annuler
+							</v-btn>
+						</v-row>
+					</v-col>
+				</v-row>
+			</v-sheet>
+		</v-bottom-sheet>
 	</div>
 </template>
 
@@ -54,11 +137,19 @@
 </style>
 
 <script>
+import Header from "@/components/graphics/Header";
+
 export default {
 	name: "Products",
+
+	components: {
+		Header
+	},
+
 	data() {
 		return {
-			
+			placeBid: false,
+			price: 500
 		}
 	},
 
