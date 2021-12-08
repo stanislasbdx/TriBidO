f64690420e255184223bf63e85df40d6b7893c9d
Kilyan <71603246+Kiky-ris@users.noreply.github.com>
2021-12-09 00:54:24 +0100

    Traduction et suppression des champs mot de passe/pseudo

--- src/views/Account.vue
+++ src/views/Account.vue
@@ -14,10 +14,10 @@
 									<v-avatar color="secondary" size="70">IMG</v-avatar>
 								</v-col>
 								<v-col class="d-flex align-center" cols="12" lg="4">
-									<v-btn class="secondary--text">Upload</v-btn>
+									<v-btn class="secondary--text">Télécharger</v-btn>
 								</v-col>
 								<v-col class="d-flex align-center" cols="12" lg="4">
-									<v-btn class="grey--text">Remove</v-btn>
+									<v-btn class="grey--text">Supprimer</v-btn>
 								</v-col>
 							</v-row>
 						</v-col>
@@ -26,45 +26,37 @@
 
 						<v-row class="pt-5 align-content-center">
 							<v-col cols="12" lg="6">
-								<v-text-field v-model="Name" label="Display name" hint="Visible to other members" persistent-hint clearable placeholder="James"></v-text-field>
+								<v-text-field v-model="Name" label="Pseudo" hint="Visible par les autres membres" persistent-hint clearable placeholder="James"></v-text-field>
 							</v-col>
 							<v-col cols="12" lg="6">
-								<v-text-field v-model="FullName" label="Full name" hint="How do you want to be called ?" persistent-hint clearable placeholder="James McDowel"></v-text-field>
+								<v-text-field v-model="FullName" label="Nom complet" hint="Comment voulez-vous être appelé ?" persistent-hint clearable placeholder="James McDowel"></v-text-field>
 							</v-col>
 						</v-row>
 
 						<v-row class=" align-content-center">
 							<v-col cols="12" lg="6">
-								<v-text-field v-model="EmailAdress" label="Email adress" hint="For notification and logging in" persistent-hint clearable></v-text-field>
+								<v-text-field v-model="EmailAdress" label="Adresse mail" hint="Recevoir les notifications et se connecter" persistent-hint clearable></v-text-field>
 							</v-col>
 							<v-col cols="12" lg="6">
-								<v-text-field v-model="PhoneNumber" label="Phone number" hint="For receiving notifications" persistent-hint clearable></v-text-field>
+								<v-text-field v-model="PhoneNumber" label="Numéro de téléphone" hint="Pour recevoir les notifications" persistent-hint clearable></v-text-field>
 							</v-col>
 						</v-row>
 						<v-row>
 							<v-col>
-								<v-text-field v-model="Adress" label="Adress" hint="For receiving notifications" persistent-hint clearable></v-text-field>
+								<v-text-field v-model="Adress" label="Adresse postale"  persistent-hint clearable></v-text-field>
 							</v-col>
 						</v-row>
 						<v-row class=" pb-10 align-content-center">
 							<v-col cols="12" lg="6">
-								<v-text-field v-model="DateBirth" label="Date of birth" hint="For notification and logging in" persistent-hint clearable></v-text-field>
+								<v-text-field v-model="DateBirth" label="Date de naissance"  persistent-hint clearable></v-text-field>
 							</v-col>
 							<v-col cols="12" lg="6">
-								<v-text-field v-model="Gender" label="Gender" hint="For receiving notifications" persistent-hint clearable></v-text-field>
+								<v-text-field v-model="Gender" label="Genre"  persistent-hint clearable></v-text-field>
 							</v-col>
 						</v-row>
 						<hr>
-						<v-row class=" pt-5 align-content-center">
-							<v-col cols="12" lg="6">
-								<v-text-field v-model="Username" label="Username" hint="For notification and logging in" persistent-hint clearable></v-text-field>
-							</v-col>
-							<v-col cols="12" lg="6">
-								<v-text-field v-model="Password" label="Password" hint="For receiving notifications" persistent-hint clearable></v-text-field>
-							</v-col>
-						</v-row>
 						<v-col class="pa-5 d-flex justify-end">
-							<v-btn @click="signup()" color="secondary" x-large>Envoyer</v-btn>
+							<v-btn @click="signup()" color="secondary" large>Envoyer</v-btn>
 						</v-col>
 					</v-card-text>
 				</v-card>
