94d369ea09768b96d390615d93c4c6d101d0a7fd
Stanislas Castaybert <perso@stan1712.com>
2021-12-08 02:26:06 +0100

    🌈 style・Centrage des actions utilisateur

--- src/App.vue
+++ src/App.vue
@@ -24,14 +24,16 @@
 					</v-col>
 				</v-row>
 
-				<v-col cols="12" class="mt-12 pt-12" v-if="user">
-					<v-icon v-if="String($route.path) == '/account'" color="primary" v-bind:large="$vuetify.breakpoint.mdAndUp" style="cursor: pointer;">
-						far fa-user-circle
-					</v-icon>
-					<v-icon v-else color="accent" v-bind:large="$vuetify.breakpoint.mdAndUp" style="cursor: pointer;" @click="$router.push('/account')">
-						far fa-user-circle
-					</v-icon>
+				<div class="userIcons">
+					<v-col cols="12" class="mt-12 pt-12" v-if="user">
+						<v-icon v-if="String($route.path) == '/account'" color="primary" v-bind:large="$vuetify.breakpoint.mdAndUp" style="cursor: pointer;">
+							far fa-user-circle
+						</v-icon>
+						<v-icon v-else color="accent" v-bind:large="$vuetify.breakpoint.mdAndUp" style="cursor: pointer;" @click="$router.push('/account')">
+							far fa-user-circle
+						</v-icon>
 
+						<v-spacer class="mt-3"></v-spacer>
 						<v-tooltip right>
 							<template v-slot:activator="{ on, attrs }">
 								<v-icon v-bind="attrs" v-on="on" :class="{'accent--text': String($route.path) != '/profile', 'primary--text': String($route.path) == '/profile'}" style="cursor: pointer;" @click="$router.push('/profile')">
@@ -40,21 +42,25 @@
 							</template>
 							<span>Accéder au profil</span>
 						</v-tooltip>
-							<v-icon v-bind="attrs" v-on="on" color="accent" style="cursor: pointer;" @click="logout()">
-								fa fa-sign-out
-							</v-icon>
-						</template>
-						<span>Se déconnecter</span>
-					</v-tooltip>
-				</v-col>
-				<v-col cols="12" class="mt-12 pt-12" v-else>
-					<v-icon v-if="String($route.path) == '/login' || String($route.path) == '/register'" color="primary" v-bind:large="$vuetify.breakpoint.mdAndUp" style="cursor: pointer;">
-						fa fa-sign-in
-					</v-icon>
-					<v-icon v-else color="accent" v-bind:large="$vuetify.breakpoint.mdAndUp" style="cursor: pointer;" @click="$router.push('/login')">
-						fa fa-sign-in
-					</v-icon>
-				</v-col>
+						<v-spacer class="my-2"></v-spacer>
+						<v-tooltip right>
+							<template v-slot:activator="{ on, attrs }">
+								<v-icon v-bind="attrs" v-on="on" color="accent" style="cursor: pointer;" @click="logout()">
+									fa fa-sign-out
+								</v-icon>
+							</template>
+							<span>Se déconnecter</span>
+						</v-tooltip>
+					</v-col>
+					<v-col cols="12" class="mt-12 pt-12" v-else>
+						<v-icon v-if="String($route.path) == '/login' || String($route.path) == '/register'" color="primary" v-bind:large="$vuetify.breakpoint.mdAndUp" style="cursor: pointer;">
+							fa fa-sign-in
+						</v-icon>
+						<v-icon v-else color="accent" v-bind:large="$vuetify.breakpoint.mdAndUp" style="cursor: pointer;" @click="$router.push('/login')">
+							fa fa-sign-in
+						</v-icon>
+					</v-col>
+				</div>
 			</div>
 		</div>
 
@@ -67,6 +73,17 @@
 </template>
 
 <style>
+	.userIcons {
+		position: absolute;
+		bottom: 3vh;
+
+		margin-left: auto;
+		margin-right: auto;
+		left: 0;
+		right: 0;
+		text-align: center;
+	}
+
 	.fade-enter,
 	.fade-leave-to {
 		opacity: 0;
