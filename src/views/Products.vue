7f9e5021461ccf873655c804b7e6b235beaa633f
Stanislas Castaybert <perso@stan1712.com>
2021-12-09 02:13:52 +0100

    ✨ feature・Refonte de la page produits

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,41 +1,38 @@
 <template>
 	<div>
-		<Header></Header>
+		<Header title="Enchères en cours"></Header>
 
 		<v-row>
-			<v-col cols="12" md="9">
+			<v-col cols="12">
 				<v-row>
-					<v-col cols="12">
-						<v-card>
-							<v-card-text>
-								<h1 class="font-weight-regular black--text">Enchères en cours</h1>
-							</v-card-text>
-						</v-card>
-					</v-col>
-
 					<v-col v-for="item in data" :key="item.id" cols="12" sm="6" md="4" xl="3">
 						<v-card class="mx-auto glasscard white--text">
-							<v-img v-if="item.avatar" height="20vh" :src="item.avatar"></v-img>
+							<v-img v-if="item.img.main" height="20vh" :src="item.img.main"></v-img>
 
 							<v-card-title class="pb-0">{{ item.name }}</v-card-title>
 
 							<v-card-text>
-								<div class="text-subtitle-1">
-									{{ item.prices.actual.toLocaleString(undefined, {minimumFractionDigits: 2}) }}€ • 32 enchères
+								<div class="text-subtitle-1 primary--text">
+									{{ item.prices.actual.toLocaleString(undefined, {minimumFractionDigits: 2}) }}€ • {{ Math.round(Math.random() * 10) }} enchères
 								</div>
+								<div class="primary--text">
+									Fin de l'enchère le {{ $moment(item.timeOptions.bidStopTime).format("DD/MM/yyyy à HH:mm") }}.
+								</div>
+
+								<v-divider style="background-color: white;" class="my-4"></v-divider>
 
 								<div>{{ item.shortDesc }}</div>
 							</v-card-text>
 
-							<v-divider class="mx-4"></v-divider>
+							<v-divider class="mx-4" style="background-color: white;"></v-divider>
 
 							<v-card-title>Dernières offres</v-card-title>
 
 							<v-card-text>
 								<v-row>
-									<v-col v-for="index in 5" :key="index" class="pb-1 pt-0">
-										<v-chip class="glass">
-											Sarahlpb {{ item.prices.actual.toLocaleString(undefined, {minimumFractionDigits: 2}) }}€
+									<v-col v-for="index in Math.round(Math.random() * 10)" :key="index" class="pb-1 pt-0">
+										<v-chip class="glass black--text">
+											{{ randomUsers[Math.floor(Math.random() * randomUsers.length)] }} {{ Math.round(item.prices.actual * (Math.random() * 5)).toLocaleString(undefined, {minimumFractionDigits: 2}) }}€
 										</v-chip>
 									</v-col>
 								</v-row>
@@ -43,14 +40,14 @@
 
 							<v-card-actions>
 								<v-row>
-									<v-col cols="12" sm="auto" align="center">
+									<v-col cols="12" sm="5" align="center">
 										<v-row>
-											<v-col cols="6" sm="auto">
-												<v-btn text block @click="placeBid = !placeBid" color="primary">
+											<v-col cols="6">
+												<v-btn text block @click="$router.push('bid/' + item.id)" color="primary">
 													Voir
 												</v-btn>
 											</v-col>
-											<v-col cols="6" sm="auto">
+											<v-col cols="6">
 												<v-btn text block @click="activeBid = item, placeBid = !placeBid" color="secondary">
 													Enchérir
 												</v-btn>
@@ -60,9 +57,9 @@
 
 									<v-spacer></v-spacer>
 
-									<v-col cols="12" sm="auto" align="center">
+									<v-col cols="12" sm="4" align="center" class="mr-3">
 										<v-row>
-											<v-col cols="6" sm="auto">
+											<v-col cols="6">
 												<v-tooltip bottom color="glass">
 													<template v-slot:activator="{ on, attrs }">
 														<v-btn text icon color="accent" v-bind="attrs" v-on="on">
@@ -72,7 +69,7 @@
 													<span>Créer un suivi</span>
 												</v-tooltip>
 											</v-col>
-											<v-col cols="6" sm="auto">
+											<v-col cols="6">
 												<v-tooltip bottom color="glass">
 													<template v-slot:activator="{ on, attrs }">
 														<v-btn text icon color="pink" v-bind="attrs" v-on="on">
@@ -90,7 +87,7 @@
 					</v-col>
 				</v-row>
 			</v-col>
-			<v-col cols="12" md="3" order="first" order-md="last">
+			<!--<v-col cols="12" md="3" order="first" order-md="last">
 				<v-row>
 					<v-col cols="12">
 						<v-card>
@@ -107,7 +104,7 @@
 						</v-card>
 					</v-col>
 				</v-row>
-			</v-col>
+			</v-col>-->
 		</v-row>
 
 		<v-bottom-sheet v-model="placeBid">
@@ -115,12 +112,12 @@
 				<v-row>
 					<v-col cols="12" md="4">
 						<v-card class="mx-auto">
-							<v-img class="white--text align-end" height="200px" :src="activeBid.avatar">
-								<v-card-title class="pb-0" v-bind:class="{'black--text': !activeBid.avatar}">{{ activeBid.name }}</v-card-title>
+							<v-img class="white--text align-end" height="200px" :src="activeBid.img.main" style="background-color: black;">
+								<v-card-title class="pb-0" v-bind:class="{'black--text': !activeBid.img.main}">{{ activeBid.name }}</v-card-title>
 
-								<v-card-text align="left" v-bind:class="{'black--text': !activeBid.avatar}">
+								<v-card-text align="left" v-bind:class="{'black--text': !activeBid.img.main}">
 									<div class="text-subtitle-1">
-										{{ activeBid.prices.actual.toLocaleString(undefined, {minimumFractionDigits: 2}) }}€ • 32 enchères
+										{{ activeBid.prices.actual.toLocaleString(undefined, {minimumFractionDigits: 2}) }}€ • {{ Math.round(activeBid.prices.actual * 10) }} enchères
 									</div>
 
 									<div>C'est un super ballon de basket basique, mais il est super !</div>
@@ -176,7 +173,9 @@ export default {
 
 			activeBid: this.$models.bid,
 
-			data: []
+			data: [],
+
+			randomUsers: ['Sarahlpb', 'psyroman212', 'stan1712', 'Kikyris', 'Sacrigmatik', 'Pierre D.', 'Over_404', '775pl', 'Bggs', 'indes33']
 		}
 	},
 
