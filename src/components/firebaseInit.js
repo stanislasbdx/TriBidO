e2fc3d1e4653bbb0859fc26a7b3333f281a2c20c
stanislasbdx <perso@stan1712.com>
2022-05-09 19:11:52 +0200

    🚀 workspace upgrade・Ajout de variables d'environnement

--- src/components/firebaseInit.js
+++ src/components/firebaseInit.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase';
 
 var config = {
-	apiKey: "AIzaSyAOZPiF8RPGBnJFppoe234nXkAA1AqRJ2E",
+	apiKey: process.env.VUE_APP_API_KEY,
 	authDomain: "tribido-236f1.firebaseapp.com",
 	databaseURL: "https://tribido-236f1-default-rtdb.europe-west1.firebasedatabase.app",
 	projectId: "tribido-236f1",
 	storageBucket: "tribido-236f1.appspot.com",
 	messagingSenderId: "854985728785",
-	appId: "1:854985728785:web:4434d47373316a402162b0",
-	measurementId: "G-ZGV8YBGH6M"
+	appId: process.env.VUE_APP_ID,
+	measurementId: process.env.VUE_APP_MEASUREMENT_ID
 };
 firebase.initializeApp(config);
 
